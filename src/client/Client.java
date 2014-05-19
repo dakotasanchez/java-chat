7f53102ecb0ebc173b085662a6a890c130652e08
dakotasanchez <dakotasanchez@outlook.com>
2014-05-18 23:36:45 -0700

    Organized structure

---
+++ src/client/Client.java
@@ -0,0 +1,413 @@
+/*Dakota Sanchez
+ * GUI Chatroom Client
+ */
+
+import java.net.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.io.*;
+
+import sun.audio.*;
+
+import javax.swing.*;
+import javax.swing.UIManager.*;
+import javax.swing.text.DefaultCaret;
+
+import java.awt.*;
+import java.awt.event.*;
+
+@SuppressWarnings("restriction")
+public class Client implements ActionListener, WindowListener {
+
+	private Socket socket;
+	private BufferedReader in;
+	private PrintWriter out;
+	private DateFormat dateFormat;
+	private Thread t;
+
+	private String name, file, date;
+	private boolean connected, kicked, canada, playAudio;
+
+	private JFrame frame;
+	private JPanel bottomPanel, topPanel;
+	private JTextArea chatArea;
+	private JTextField textField;
+	private JButton canadaButton, muteButton, alertButton;
+	private JLabel label;
+	private DefaultCaret caret;
+
+	public static void main(String[] args) throws IOException {
+
+		new Client();
+	}
+
+	public Client() throws IOException {
+
+		connected = false;
+		kicked = false;
+		canada = false;
+		playAudio = true;
+		file = "alert.wav";
+
+		dateFormat = new SimpleDateFormat("HH:mm");
+
+		frame = new JFrame();
+		frame.setLayout(new BorderLayout());
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.setTitle("Dakota Sanchez Chatroom Project");
+		frame.setSize(600, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.addWindowListener(this);
+
+		setLAF();
+		setComponents();
+		frame.setVisible(true);
+
+		connect("Please enter a username:");
+	}
+
+	//Set user interface components
+	public void setComponents() {
+
+		bottomPanel = new JPanel(new FlowLayout(FlowLayout.LEADING, 5, 5));
+		bottomPanel.setBackground(new Color(55, 55, 55));
+
+		topPanel = new JPanel();
+		topPanel.setBackground(new Color(55, 55, 55));
+
+		canadaButton = new JButton("Translate to Canadian: OFF");
+		canadaButton.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		canadaButton.setBackground(new Color(30, 30, 30));
+		canadaButton.setForeground(Color.WHITE);
+
+		canadaButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				if(canada) {
+					canada = false;
+					canadaButton.setText("Translate to Canadian: OFF");
+					canadaButton.setBackground(new Color(30, 30, 30));
+				} else {
+					canada = true;
+					canadaButton.setText("Translate to Canadian: ON");
+					canadaButton.setBackground(new Color(150,20,20));
+				}
+			}
+		});
+
+		muteButton = new JButton("Mute: OFF");
+		muteButton.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		muteButton.setBackground(new Color(30,30,30));
+		muteButton.setForeground(Color.WHITE);
+
+		muteButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				if(playAudio) {
+					playAudio = false;
+					muteButton.setText("Mute: ON");
+					muteButton.setBackground(new Color(150,20,20));
+				} else {
+					playAudio = true;
+					muteButton.setText("Mute: OFF");
+					muteButton.setBackground(new Color(30,30,30));
+				}
+			}
+		});
+
+		alertButton = new JButton("Alert Sound: 1");
+		alertButton.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		alertButton.setBackground(new Color(0, 34, 102));
+		alertButton.setForeground(Color.WHITE);
+
+		alertButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				if(alertButton.getText().endsWith("1")) {
+					alertButton.setText("Alert Sound: 2");
+					file = "alert2.wav";
+					if(playAudio)
+						new AudioClip().start();
+				} else if(alertButton.getText().endsWith("2")) {
+					alertButton.setText("Alert Sound: 3");
+					file = "alert3.wav";
+					if(playAudio)
+						new AudioClip().start();
+				} else {
+					alertButton.setText("Alert Sound: 1");
+					file = "alert.wav";
+					if(playAudio)
+						new AudioClip().start();
+				}
+			}
+		});
+
+		label = new JLabel("                         ");
+		label.setForeground(new Color(255, 255, 255));
+
+		textField = new JTextField(40);
+		textField.setVisible(false);
+		textField.addActionListener(this);
+
+		//Set main chat area
+		chatArea = new JTextArea();
+		chatArea.setBackground(new Color(220, 220, 220));
+		chatArea.setFont(new Font("SansSerif", Font.BOLD, 13));
+		chatArea.setEditable(false);
+		chatArea.setLineWrap(true);
+		chatArea.setWrapStyleWord(true);
+
+		caret = (DefaultCaret)chatArea.getCaret();
+		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+
+		bottomPanel.add(label);
+		bottomPanel.add(textField);
+
+		topPanel.add(canadaButton);
+		topPanel.add(muteButton);
+		topPanel.add(alertButton);
+
+		frame.add(new JScrollPane(chatArea), BorderLayout.CENTER);
+		frame.add(bottomPanel, BorderLayout.SOUTH);
+		frame.add(topPanel, BorderLayout.NORTH);
+	}
+
+	//Change Look & Feel of user interface
+	public void setLAF() {
+
+		try{
+			for(LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	//Add 'eh' suffix to strings
+	public String canadianString(String s) {
+
+		if(s.endsWith(".") || s.endsWith("?") || s.endsWith("!")) {
+			String end = s.substring(s.length() - 1);
+			int ends = 0;
+			while(s.substring(s.length() - 1).equals(end)) {
+				s = s.substring(0, s.length() - 1);
+				ends++;
+			}
+			s = s + " eh";
+			for(int i = 0; i < ends; i++) {
+				s = s + end;
+			}
+		} else {
+			s = s + " eh";
+		}
+
+		return s;
+	}
+
+	//Get date
+	public String getDate()	{
+
+		date = dateFormat.format(new Date());
+
+		if(date.startsWith("0"))
+			date = date.substring(1) + "am";
+		else if(Integer.parseInt((date.substring(0, 2))) < 12)
+			date = date + "am";
+		else if(Integer.parseInt((date.substring(0, 2))) == 12)
+			date = date + "pm";
+		else
+			date = (Integer.parseInt(date.substring(0, 2)) - 12) + date.substring(2) + "pm";
+
+		return date;
+	}
+
+	//Kick this user from the chatroom
+	public void kick() throws IOException{
+
+		out.println("CloseStreams");
+		out.close();
+		in.close();
+		socket.close();
+		chatArea.append("\nYou have been kicked by the Server.\n");
+		chatArea.setForeground(Color.RED);
+		kicked = true;
+	}
+
+	//Send data from text field when 'return' is pressed
+	public void actionPerformed(ActionEvent evt) {
+
+		if(textField.getText().length() > 0 && !kicked) {
+
+			String s = textField.getText();
+			out.println(s);
+
+			//Add "eh" if "Canadian" button selected
+			if(canada) {
+				s = canadianString(s);
+			}
+
+			//Print own message on scrollpane
+			chatArea.append(getDate() + " " + name + "(Me): " + s + "\n");
+		}
+
+		textField.setText("");
+	}
+
+	//Handles all incoming data from server.
+	private class Input extends Thread {
+
+		public void run() {
+
+			while(!kicked) {
+				try {
+					String s = in.readLine();
+
+					//Check to see if Server is trying to remove user
+					if(!s.equals("AdminRemovalMessage")) {
+
+						//Add "eh" to incoming messages
+						if(canada) {
+							s = canadianString(s);
+						}
+
+						if(playAudio) {
+							new AudioClip().start();
+						}
+
+						chatArea.append(getDate() + " " + s + "\n");
+						chatArea.setCaretPosition(chatArea.getDocument().getLength());
+
+					} else {
+
+						kick();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+					break;
+				}
+			}
+		}
+	}
+
+	//Handles alert sounds
+	private class AudioClip extends Thread implements ActionListener {
+
+		private AudioStream audioStream;
+
+		public void run() {
+
+			//Open audio stream
+			try {
+				audioStream = new AudioStream(this.getClass().getResourceAsStream("/res/" + file));
+				AudioPlayer.player.start(audioStream);
+
+				Timer time = new Timer(500, this);
+				time.setRepeats(false);
+				time.start();
+
+			} catch(FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void actionPerformed(ActionEvent event) {
+
+			AudioPlayer.player.stop(audioStream);
+		}
+	}
+
+	//Attempt to establish connection to server on entered address
+	public void connect(String message) {
+
+		try {
+
+			name = (String)JOptionPane.showInputDialog(
+					null,
+					"",
+					message,
+					JOptionPane.PLAIN_MESSAGE,
+					null,
+					null,
+					"");
+
+			if(name == null) System.exit(0);
+
+			socket = new Socket(InetAddress.getLocalHost(), 55555);
+
+			connected = true;
+
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream(), true);
+
+			out.println(name);
+
+			label.setText(" Chat as you please: ");
+			label.repaint();
+			textField.setVisible(true);
+			textField.repaint();
+
+			chatArea.append("Welcome " + name + "!\n");
+			chatArea.append("You have connected to the chatroom.\n\n");
+			out.println("FIRSTCONNECTSERVER: " + name + " has connected to the chatroom.\n");
+
+
+			t = new Thread(new Input());
+			t.setDaemon(true);
+			t.start();
+
+			textField.requestFocus();
+
+		} catch (IOException e) {
+			try {
+				if(out != null) {out.close();}
+				if(in != null) {in.close();}
+				if(socket != null) {socket.close();}
+
+				connect("Server is experiencing issues :(");
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	public void windowClosing(WindowEvent arg0) {
+
+		//Let Server knoww to remove this user
+		if(out != null)
+			out.println("EXITINGCHATROOM");
+
+		//Clean up references
+		try {
+			if(out != null)
+				out.close();
+			if(in != null)
+				in.close();
+			if(socket != null)
+				socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		frame.dispose();
+		System.exit(0);
+	}
+
+	//Non-implemented methods for WindowListener interface
+	public void windowActivated(WindowEvent arg0) {}
+
+	public void windowClosed(WindowEvent arg0) {}
+
+	public void windowDeactivated(WindowEvent arg0) {}
+
+	public void windowDeiconified(WindowEvent arg0) {}
+
+	public void windowIconified(WindowEvent arg0) {}
+
+	public void windowOpened(WindowEvent arg0) {}
+
+}
---
+++ src/server/Server.java
@@ -0,0 +1,181 @@
+/*
+ * Dakota Sanchez
+ * Chatroom Socket Server
+ */
+
+import java.net.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+//This handles all of the Clients in the Server.
+public class Server {
+	
+	private ServerSocket serverSocket;
+	private Socket socket;
+	private ArrayList<User> users;
+	private Thread thread;
+	private PrintWriter logWriter;
+	private DateFormat dateFormat;
+	private Date date;
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		
+		new Server();
+	}
+
+	//Creates a new Server object
+	public Server() throws UnknownHostException, IOException {
+		
+		//Attempt to create server socket
+		try {
+			
+			serverSocket = new ServerSocket(55555);
+
+		} catch(IOException e){
+			System.out.println("Could not open server socket.");
+			e.printStackTrace();
+			return;
+		}
+
+		//Notify which address to connect to
+		System.out.println("Socket " + serverSocket + " created.\nConnect to: " + 
+				InetAddress.getLocalHost().getHostAddress());
+		
+		users = new ArrayList<User>();
+		
+		//Create a new Connect Object
+		thread = new Thread(new Connect());
+		thread.start();
+	}
+	
+	//The data stream for each client
+	private class Outport extends Thread {
+		
+		private Socket socket;
+		private BufferedReader in;
+		private PrintWriter out;
+		private User thisUser;
+		
+		public Outport(Socket newSocket) {
+			socket = newSocket;
+			start();
+		}
+		
+		public void run() {
+			
+			try {
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				out = new PrintWriter(socket.getOutputStream(), true);
+				
+				String exit = in.readLine();
+				
+				if(!exit.equals("EXITINGCHATROOM")) {
+					thisUser = new User(exit, out);
+					users.add(thisUser);
+				}
+				
+				while(true) {					
+					
+					String str = in.readLine();	
+						
+					if(str != null) {
+						
+						//Kick
+						if(str.equalsIgnoreCase("CloseStreams")) {
+							System.out.println("Client " + socket + " has disconnected.");
+							users.remove(users.indexOf(thisUser));
+
+							for(User u : users) {
+								if(u != thisUser)
+									u.writer().println("SERVER: " + thisUser.name() +
+											" has been kicked from the server, laugh at their misfortune.");
+							}
+							in.close();
+							out.close();
+							socket.close();
+							thisUser.closeWriter();
+							break;
+						}
+						
+						if(str.length() > 0) {
+							
+							//Send data to every client
+							for(User u : users) {
+								
+								if(u != thisUser) {
+
+									if(str.equals("EXITINGCHATROOM"))
+										u.writer().println("SERVER: " + thisUser.name() + " has disconnected");							
+									else if(str.contains("FIRSTCONNECT"))
+										u.writer().println(str.substring(12));
+									else
+										u.writer().println(thisUser.name() + ": " + str);								
+								}
+							}	
+
+							if(str.equals("EXITINGCHATROOM")) {
+								System.out.println("Client " + socket + " has disconnected.");
+								users.remove(users.indexOf(thisUser));
+								break;							
+							}					
+						}
+					}					
+				}
+
+			}catch(IOException e) {
+				System.out.println("Client " + socket + " has disconnected.");
+				users.remove(users.indexOf(thisUser));
+			}
+		}
+	}
+	
+	//Starts the client accepting process
+	private class Connect extends Thread {
+		
+		public void run() {
+			
+			System.out.println("Server has been started.");
+
+			while(true) {
+								
+				try {
+					socket = serverSocket.accept();
+					if(socket != null) {
+						System.out.println("Client " + socket + " has connected.");
+						new Outport(socket);
+					}
+				}catch(IOException e) {
+					e.printStackTrace();
+				}			
+			}
+		}
+	}
+	
+	//Represents a client
+	private class User {
+		
+		private String name;
+		private PrintWriter out;
+		
+		public User(String name, PrintWriter out) {
+			this.name = name;
+			this.out = out;
+		}
+		
+		public String name() {
+			return name;
+		}
+		
+		public PrintWriter writer() {
+			return out;
+		}
+
+		public void closeWriter() {
+			out.close();
+		}
+	}
+}
